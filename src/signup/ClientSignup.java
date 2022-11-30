76f01d9274aa26660c90d675ca4bb4b1e8ecde52
yeah-zzi <kyj54616@gmail.com>
2022-11-30 15:03:35 +0900

    테스트 수정

--- src/signup/ClientSignup.java
+++ src/signup/ClientSignup.java
@@ -1,4 +1,5 @@
 package signup;
+// 아무거나
 
 import java.awt.*;
 import javax.swing.*;
@@ -8,8 +9,8 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 	JTextField id, pwans, name, birth, tel_num1, tel_num2, email_id;
 	JPasswordField pw;
-	String pwhint[] = { "내가 가장 존경하는 인물은?", "내가 태어난 곳은?", "내 어릴적 별명은?", "내가 키우던 애완동물의 이름은?", "내가 가장 좋아하는 간식은?",
-			"내가 싫어하는 음식은?", "내가 좋아하는 캐릭터는?", "나와 가장 친한 친구의 이름은?", "자신의 보물 1호는?", "내가 다닌 초등학교 이름은?" };
+	String pwhint[] = { "�궡媛� 媛��옣 議닿꼍�븯�뒗 �씤臾쇱�?", "�궡媛� �깭�뼱�궃 怨녹�?", "�궡 �뼱由댁쟻 蹂꾨챸��?", "�궡媛� �궎�슦�뜕 �븷�셿�룞臾쇱쓽 �씠由꾩�?", "�궡媛� 媛��옣 醫뗭븘�븯�뒗 媛꾩떇��?",
+			"�궡媛� �떕�뼱�븯�뒗 �쓬�떇��?", "�궡媛� 醫뗭븘�븯�뒗 罹먮┃�꽣�뒗?", "�굹�� 媛��옣 移쒗븳 移쒓뎄�쓽 �씠由꾩�?", "�옄�떊�쓽 蹂대Ъ 1�샇�뒗?", "�궡媛� �떎�땶 珥덈벑�븰援� �씠由꾩�?" };
 	String telcode[] = { "010", "011", "017", "070", "02", "031", "032", "033", "041" };
 	String emcode[] = { "naver.com", "gmail.com", "nate.com", "daum.net", "korea.kr" };
 	JComboBox hint, tel, email;
@@ -26,7 +27,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 		
 		
 		ImageIcon client = new ImageIcon("images/client.png");
-		// imageicon 크기 조정을 위해 image 객체로 변경하여 크기 조정 후, 다시 imageicon화 함.
+		// imageicon �겕湲� 議곗젙�쓣 �쐞�빐 image 媛앹껜濡� 蹂�寃쏀븯�뿬 �겕湲� 議곗젙 �썑, �떎�떆 imageicon�솕 �븿.
 		Image clientImg = client.getImage();
 		Image ImgClient = clientImg.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
 		ImageIcon ClientIcon = new ImageIcon(ImgClient);
@@ -39,20 +40,20 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p1 = new JPanel();
 		p1.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l1 = new JLabel("아이디");
+		JLabel l1 = new JLabel("�븘�씠�뵒");
 		p1.add(l1);
 
 		JPanel p2 = new JPanel();
 		p2.setLayout(new FlowLayout(FlowLayout.LEFT));
-		id = new JTextField("6자리 (영소문자&숫자)", 15);
-		check = new JButton("중복 확인");
+		id = new JTextField("6�옄由� (�쁺�냼臾몄옄&�닽�옄)", 15);
+		check = new JButton("以묐났 �솗�씤");
 		check.addActionListener(this);
 		p2.add(id);
 		p2.add(check);
 
 		JPanel p3 = new JPanel();
 		p3.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l2 = new JLabel("비밀번호");
+		JLabel l2 = new JLabel("鍮꾨�踰덊샇");
 		p3.add(l2);
 
 		JPanel p4 = new JPanel();
@@ -62,7 +63,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p5 = new JPanel();
 		p5.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l3 = new JLabel("비밀번호 힌트");
+		JLabel l3 = new JLabel("鍮꾨�踰덊샇 �엺�듃");
 		p5.add(l3);
 
 		JPanel p6 = new JPanel();
@@ -72,7 +73,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p7 = new JPanel();
 		p7.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l4 = new JLabel("비밀번호 힌트 답");
+		JLabel l4 = new JLabel("鍮꾨�踰덊샇 �엺�듃 �떟");
 		p7.add(l4);
 
 		JPanel p8 = new JPanel();
@@ -82,8 +83,8 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p9 = new JPanel();
 		p9.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l5 = new JLabel("이름                      ");
-		JLabel l6 = new JLabel("생년월일              ");
+		JLabel l5 = new JLabel("�씠由�                      ");
+		JLabel l6 = new JLabel("�깮�뀈�썡�씪              ");
 		p9.add(l5);
 		p9.add(l6);
 
@@ -96,7 +97,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p11 = new JPanel();
 		p11.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l7 = new JLabel("연락처");
+		JLabel l7 = new JLabel("�뿰�씫泥�");
 		p11.add(l7);
 
 		JPanel p12 = new JPanel();
@@ -110,7 +111,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 		JPanel p13 = new JPanel();
 		p13.setLayout(new FlowLayout(FlowLayout.LEFT));
-		JLabel l8 = new JLabel("이메일");
+		JLabel l8 = new JLabel("�씠硫붿씪");
 		p13.add(l8);
 
 		JPanel p14 = new JPanel();
@@ -127,8 +128,8 @@ public class ClientSignup extends JFrame implements ActionListener {
 		ct.add(top, BorderLayout.CENTER);
 
 		JPanel bottom = new JPanel();
-		ok = new JButton("확인");
-		cancel = new JButton("취소");
+		ok = new JButton("�솗�씤");
+		cancel = new JButton("痍⑥냼");
 		cancel.addActionListener(this);
 		bottom.add(ok);
 		bottom.add(cancel);
@@ -138,7 +139,7 @@ public class ClientSignup extends JFrame implements ActionListener {
 
 	public void actionPerformed(ActionEvent ae) {
 		String s = ae.getActionCommand();
-		if (s.equals("취소")) {
+		if (s.equals("痍⑥냼")) {
 			id.setText("");
 			pw.setText("");
 			pwans.setText("");
@@ -150,16 +151,16 @@ public class ClientSignup extends JFrame implements ActionListener {
 			hint.setSelectedItem((Object) pwhint[0]);
 			tel.setSelectedItem((Object) telcode[0]);
 			email.setSelectedItem((Object) emcode[0]);
-		} // 취소 버튼 누르면 모든 입력값 초기화
-		else if (s.equals("확인")) {
-		} // 확인 버튼 누르면 DB연동 저장
+		} // 痍⑥냼 踰꾪듉 �늻瑜대㈃ 紐⑤뱺 �엯�젰媛� 珥덇린�솕
+		else if (s.equals("�솗�씤")) {
+		} // �솗�씤 踰꾪듉 �늻瑜대㈃ DB�뿰�룞 ���옣
 		else {
 
-		} // 아이디 중복확인 DB로 확인 후, 사용가능여부 창 띄우기(JDiglog)
+		} // �븘�씠�뵒 以묐났�솗�씤 DB濡� �솗�씤 �썑, �궗�슜媛��뒫�뿬遺� 李� �쓣�슦湲�(JDiglog)
 	}
 
 	public static void main(String[] args) {
-		ClientSignup win = new ClientSignup("고객 회원가입");
+		ClientSignup win = new ClientSignup("怨좉컼 �쉶�썝媛��엯");
 		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		win.setSize(700, 800);
 		win.setLocation(350, 80);
